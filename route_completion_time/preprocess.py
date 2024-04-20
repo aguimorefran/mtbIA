6efb37d44f80f790c20a8f293bfbd1d219765cbc
Fco <aguimorefran@gmail.com>
2024-04-21 00:57:03 +0200

    + Change preprocessor

--- route_completion_time/preprocess.py
+++ route_completion_time/preprocess.py
@@ -1,74 +1,109 @@
 import numpy as np
 import pandas as pd
+import tqdm
 
-# Preprocessing
-processed_activities_file = "../data/processed_activities.csv"
+processed_activities_file = "../data/ready_data.csv"
 df_activity = pd.read_csv(processed_activities_file)
 
-columns_to_keep = [
+COLS_TO_KEEP = [
     "id",
     "timestamp",
     "position_lat",
     "position_long",
     "distance",
-    "altitude",
-    "grade"
+    "altitude"
 ]
 
+GRADE_CUTS = [-np.inf, -1, 4, 8, 12, 20, np.inf]
+GRADE_LABELS = [
+    "downhill",
+    "green",
+    "yellow",
+    "orange",
+    "red",
+    "black"
+]
 
-def get_season(timestamp):
-    month = timestamp.month
-    if month in [12, 1, 2]:
-        return "winter"
-    elif month in [3, 4, 5]:
-        return "spring"
-    elif month in [6, 7, 8]:
-        return "summer"
-    else:
-        return "fall"
+YEAR_SEASONS = {
+    1: "winter",
+    2: "winter",
+    3: "spring",
+    4: "spring",
+    5: "spring",
+    6: "summer",
+    7: "summer",
+    8: "summer",
+    9: "fall",
+    10: "fall",
+    11: "fall",
+    12: "winter"
+}
 
+TIME_OF_DAY_CUTS = [0, 6, 12, 18, 24]
+TIME_OF_DAY_LABELS = [
+    "night",
+    "morning",
+    "afternoon",
+    "evening"
+]
 
-def time_of_day(timestamp):
-    hour = timestamp.hour
-    if hour in range(6, 12):
-        return "morning"
-    elif hour in range(12, 18):
-        return "afternoon"
-    else:
-        return "evening"
-
-
-def ascent_meters(altitude):
-    ascent = np.sum(np.maximum(altitude - np.roll(altitude, 1), 0))
-    return ascent
-
-
-def categorize_grade(df):
-    cuts = [-float("inf"), -10, -5, 5, 10, 20, float("inf")]
-    labels = ["hard_dh", "dh", "flat", "up", "hard_up", "extreme_up"]
-    df["grade_cat"] = pd.cut(df["grade"], bins=cuts, labels=labels)
+def preprocess(data):
+    print("Preprocessing data...")
+    df = data.copy()
+    df = df[COLS_TO_KEEP]
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df["elapsed"] = (df["timestamp"] - df["timestamp"].min()).dt.total_seconds()
+    df["season"] = df["timestamp"].dt.month.map(YEAR_SEASONS)
+    df["time_of_day"] = pd.cut(df["timestamp"].dt.hour, bins=TIME_OF_DAY_CUTS, labels=TIME_OF_DAY_LABELS)
+    df["distance_diff"] = df["distance"].diff()
+    df["altitude_diff"] = df["altitude"].diff()
+    df["grade"] = df["altitude_diff"] / df["distance_diff"]
+    df["grade"] = df["grade"].fillna(0)
+    df["altitude_diff"] = df["altitude_diff"].fillna(0)
+    df["distance_diff"] = df["distance_diff"].fillna(0)
+    df["grade_category"] = pd.cut(df["grade"], bins=GRADE_CUTS, labels=GRADE_LABELS)
     return df
 
+def aggregate_by_id(data, contains_time):
+    # New columns
+    # - distance_meters = sum(distance)
+    # - ascent_meters = sum of positive altitude_diff
+    # - elapsed_minutes = max(elapsed) / 60
+    # - season = mode(season)
+    # - time_of_day = mode(time_of_day)
+    # - {grade_cat}_pctg = sum(distance_diff[grade_category == grade_cat])
 
-df_activity = df_activity[columns_to_keep]
-df_activity["timestamp"] = pd.to_datetime(df_activity["timestamp"])
-df_activity = df_activity.sort_values(by="timestamp")
-df_activity = categorize_grade(df_activity)
-df_activity["diff_meters"] = df_activity["distance"].diff()
+    print("Aggregating data...")
+    if contains_time:
+        df_agg = data.groupby("id").agg(
+            distance_meters=("distance", "sum"),
+            ascent_meters=("altitude_diff", lambda x: sum(x[x > 0])),
+            elapsed_minutes=("elapsed", lambda x: max(x) / 60),
+            season=("season", lambda x: x.mode().values[0]),
+            time_of_day=("time_of_day", lambda x: x.mode().values[0])
+        ).reset_index()
+    else:
+        df_agg = data.groupby("id").agg(
+            distance_meters=("distance", "sum"),
+            ascent_meters=("altitude_diff", lambda x: sum(x[x > 0])),
+            season=("season", lambda x: x.mode().values[0]),
+            time_of_day=("time_of_day", lambda x: x.mode().values[0])
+        ).reset_index()
 
-# Group by id
-df_agg = df_activity.groupby("id")
-df_agg = df_agg.agg(
-    duration_minutes=("timestamp", lambda x: (x.iloc[-1] - x.iloc[0]).total_seconds() / 60),
-    distance_meters=("distance", "max"),
-    ascent_meters=("altitude", ascent_meters),
-    time_of_day=("timestamp", lambda x: time_of_day(x.iloc[0])),
-    year_season=("timestamp", lambda x: get_season(x.iloc[0]))
-)
+    grade_cat_pivot = data.pivot_table(observed=False, index='id', columns='grade_category', values='distance', aggfunc='sum', fill_value=0)
+    grade_cat_pctg = grade_cat_pivot.div(grade_cat_pivot.sum(axis=1), axis=0)
+    grade_cat_pctg.columns = [f'{col}_pctg' for col in grade_cat_pctg.columns]
+    df_agg = df_agg.merge(grade_cat_pctg, left_on='id', right_index=True)
 
-grade_grouped = df_activity.groupby(["id", "grade_cat"], observed=False)["diff_meters"].sum()
-grade_grouped = grade_grouped.groupby(level=0).apply(lambda x: 100 * x / x.sum()).unstack()
-grade_grouped.columns = [f"{col}_pctg" for col in grade_grouped.columns]
-df_agg = df_agg.join(grade_grouped)
 
-df_agg.to_csv("preprocessed.csv")
+    return df_agg
+
+def preprocess_and_aggregate(data, contains_time):
+    df = preprocess(data)
+    df = aggregate_by_id(df, contains_time)
+
+    # Save in preprocessed.csv
+    df.to_csv("preprocessed.csv", index=False)
+    print("Preprocessed data saved in preprocessed.csv")
+
+preprocess_and_aggregate(df_activity, contains_time=True)
