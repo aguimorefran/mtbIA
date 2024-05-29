046cb6d54bf78fac298d6322e6bb0f59483bafce
aguimorefran <aguimorefran@gmail.com>
2024-05-29 12:30:07 +0200

    + Train LSTM with power and time data

--- app/power/train_power.py
+++ app/power/train_power.py
@@ -1,5 +1,3 @@
-import sqlite3
-from io import StringIO
 import pandas as pd
 import numpy as np
 import logging
@@ -7,8 +5,9 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense, LSTM, BatchNormalization, Dropout
 from tensorflow.keras.optimizers import Adam
 from datetime import datetime
 import os
@@ -19,13 +18,14 @@ logger = logging.getLogger(__name__)
 
 # Constants
 RAW_DATA_PATH = "data/activity_data_raw.csv"
+PLANNED_WORKOUTS_PATH = "data/planned_workouts.csv"
 SEQ_LENGTH = 10
 BATCH_SIZE = 32
 EPOCHS = 50
-TARGET_FEATURES = ["power", "seconds_from_start"]
+TARGET_FEATURES = ["power_3s_avg", "seconds_from_start"]
 FEATURES = ["grade", "ascent_meters", "distance_meters", "atl_start", "ctl_start", "watt_kg", "temperature"]
 ALL_FEATURES = TARGET_FEATURES + FEATURES
-MODEL_PATH = "models/LSTM_power"
+MODEL_PATH = "models/LSTM_power.keras"
 METRICS_PATH = "data/model_metrics_power.csv"
 
 def load_activity_data(csv_path):
@@ -33,10 +33,9 @@ def load_activity_data(csv_path):
     df = pd.read_csv(csv_path)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     logger.info("Data loaded successfully")
-
     return df
 
-def preprocess_data(df):
+def preprocess_data(df, drop_activities_with_workout=False):
     logger.info("Starting data preprocessing")
     df = df.sort_values(by=["activity_id", "timestamp"])
     df["altitude_diff"] = df["altitude"].diff().fillna(0)
@@ -44,30 +43,76 @@ def preprocess_data(df):
     df["seconds_from_start"] = (df["timestamp"] - df["timestamp"].iloc[0]).dt.total_seconds()
     df.rename(columns={"distance": "distance_meters"}, inplace=True)
     scaler = StandardScaler()
+
+    # Fill NaN values with a middle value between the previous and next values
+    df.fillna(method='ffill', inplace=True)
+
+    # Calculate 3-second moving average for power
+    df["power_3s_avg"] = df["power"].rolling(window=3, min_periods=1).mean()
+
+    # Load planned workouts
+    planned_workouts = pd.read_csv(PLANNED_WORKOUTS_PATH)
+
+    # If drop_activities_with_workout is True, drop activities whose id appear in planned_workouts["paired_activity_id"]
+    if drop_activities_with_workout:
+        intersecting_activities = planned_workouts["paired_activity_id"].unique()
+        logger.info(f"Dropping {len(intersecting_activities)} activities with planned workouts")
+        df = df[~df["activity_id"].isin(intersecting_activities)]
+        logger.info(f"Remaining activities: {df['activity_id'].nunique()}")
+
     df[ALL_FEATURES] = scaler.fit_transform(df[ALL_FEATURES])
+
+    rows_na = df.isna().sum().sum()
+    df.dropna(inplace=True)
+    logger.info(f"Dropped {rows_na} rows with NaN values")
+    if df[ALL_FEATURES].isnull().values.any() or np.isinf(df[ALL_FEATURES]).values.any():
+        logger.error("NaN or infinite values found in the data")
+        raise ValueError("NaN or infinite values found in the data")
+
     logger.info("Data preprocessing completed")
     return df
 
-def create_sequences(data, sequence_length, target_columns):
-    logger.info("Creating sequences for the model")
-    sequences, targets = [], []
-    for i in range(len(data) - sequence_length):
-        seq = data.iloc[i:i + sequence_length][FEATURES].values.astype('float32')
-        target = data.iloc[i + sequence_length][target_columns].values.astype('float32')
-        sequences.append(seq)
-        targets.append(target)
-    logger.info("Sequences created successfully")
-    return np.array(sequences), np.array(targets)
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-def build_model(input_shape, output_shape):
-    logger.info("Building the LSTM model")
+def create_sequences(data, sequence_length, target_columns):
+    num_records = len(data)
+    num_features = len(FEATURES)
+    num_targets = len(target_columns)
+
+    sequences = np.zeros((num_records - sequence_length, sequence_length, num_features), dtype=np.float32)
+    targets = np.zeros((num_records - sequence_length, num_targets), dtype=np.float32)
+
+    feature_data = data[FEATURES].values
+    target_data = data[target_columns].values
+
+    for i in range(num_records - sequence_length):
+        sequences[i] = feature_data[i:i + sequence_length]
+        targets[i] = target_data[i + sequence_length]
+
+    return sequences, targets
+
+
+def build_model(units=100, dropout_rate=0.2, learning_rate=0.001):
+    logger.info(
+        f"Building the LSTM model with units={units}, dropout_rate={dropout_rate}, learning_rate={learning_rate}")
     model = Sequential()
-    model.add(LSTM(50, activation="relu", input_shape=input_shape, return_sequences=True))
-    model.add(LSTM(50))
-    model.add(Dense(output_shape))
-    model.compile(optimizer=Adam(learning_rate=0.001), loss="mse")
+    model.add(LSTM(units, activation="relu", input_shape=(SEQ_LENGTH, len(FEATURES)), return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout_rate))
+    model.add(LSTM(units, activation="relu", return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout_rate))
+    model.add(LSTM(units))
+    model.add(BatchNormalization())
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(len(TARGET_FEATURES)))
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss="mse")
     logger.info("LSTM model built and compiled")
     return model
+
 
 def plot_predictions(y_test, y_pred):
     plt.figure(figsize=(14, 5))
@@ -81,10 +126,13 @@ def plot_predictions(y_test, y_pred):
     plt.legend()
     plt.show()
 
+
 def save_model(model, path):
+    ensure_dir(path)
     logger.info(f"Saving model to {path}")
     model.save(path)
     logger.info("Model saved successfully")
+
 
 def save_metrics(metrics, path):
     logger.info(f"Saving metrics to {path}")
@@ -95,14 +143,15 @@ def save_metrics(metrics, path):
         df_metrics.to_csv(path, index=False)
     logger.info("Metrics saved successfully")
 
+
 def main():
-
     df = load_activity_data(RAW_DATA_PATH)
-    df = preprocess_data(df)
+    df = preprocess_data(df, drop_activities_with_workout=True)
 
+    logger.info(f"Creating sequences with length {SEQ_LENGTH}")
     all_sequences, all_targets = [], []
-    for activity_id in df["activity_id"].unique():
-        activity_data = df[df["activity_id"] == activity_id]
+    for id in df["activity_id"].unique():
+        activity_data = df[df["activity_id"] == id]
         sequences, targets = create_sequences(activity_data, SEQ_LENGTH, TARGET_FEATURES)
         all_sequences.append(sequences)
         all_targets.append(targets)
@@ -114,14 +163,16 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = build_model((SEQ_LENGTH, len(FEATURES)), len(TARGET_FEATURES))
-    logger.info(model.summary())
+    # Use the best hyperparameters found
+    best_hyperparams = {'units': 50, 'dropout_rate': 0.2, 'learning_rate': 0.001}
+    logger.info(f"Training with best hyperparameters: {best_hyperparams}")
 
-    history = model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, valactivity_idation_data=(X_test, y_test))
+    model = build_model(**best_hyperparams)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    history = model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_test, y_test),
+                        callbacks=[early_stopping])
 
     val_loss = model.evaluate(X_test, y_test)
-    logger.info(f'Valactivity_idation Loss: {val_loss}')
-
     y_pred = model.predict(X_test)
 
     # Calculate metrics
@@ -144,5 +195,7 @@ def main():
 
     plot_predictions(y_test, y_pred)
 
+
 if __name__ == "__main__":
     main()
+
